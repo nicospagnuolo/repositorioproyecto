59dd9ff790cd33df023e9c9fa213a5b1ee9d6846
nicospagnuolo <nspagnuolo@udesa.edu.ar>
2023-05-17 14:50:30 -0300

    g

--- Proyecto/controllers/controllerProduct.js
+++ Proyecto/controllers/controllerProduct.js
@@ -1,7 +1,14 @@
-
+const productos = require('../')
 const controlador = {
+	lista:function(req,res){
+		res.render('listaProductos',{productos:productos.index,criterio:'index',})
+	},
 	index: function (req, res) {
-   	return res.render('product');
+		let id = req.params.id;
+		for(let index = 0;index<bandas.lista.lenght;index++){
+			if(id == productos.lista[index])
+		}
+   	return res.render('pruduct',{producto:productos.lista[index]});
 	 
 },
 	results:function (req, res) {
--- Proyecto/routes/users.js
+++ Proyecto/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const indexController = require('../controllers/users')
-  router.get('/',indexController.index) ;
+  router.get('/users/id/:id',indexController.index) ;
   router.get('/login',indexController.login );
   router.get('/profileedit',indexController.profileedit );
   router.get('/register',indexController.register );
