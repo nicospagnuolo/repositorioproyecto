dff13b3f97b3d1880d36e4f07e5d37802936df64
jcaldelas <jcaldelas@udesa.edu.ar>
2023-06-05 10:45:52 -0300

    ca

--- Proyecto/controllers/usersControllers.js
+++ Proyecto/controllers/usersControllers.js
@@ -9,45 +9,47 @@ const usersController = {
         return res.render('profile-edit', {user:data.user})
     },
     add:function (req, res) {
-        res.render("register")
-    },
-    store:(req, res) => {
-        let errors = {};
-        if (req.body.username == "") {
-          errors.message = "El campo username está vacío";
+      res.render('register')
+  },
+  store:(req, res) => {
+      let errors = {};
+      if (req.body.username == "") {
+        errors.message = "El campo username está vacío";
+        res.locals.errors = errors;
+        res.render("register");
+      } else if (req.body.email == "") {
+        errors.message = "El campo email está vacío";
+        res.locals.errors = errors;
+        res.render("register");
+      } else if (req.body.password =="") {
+        errors.message = "La contraseña debe tener mas de 3 caracteres.";
+        res.locals.errors = errors;
+        res.render("register");
+      } else if (req.body.perfil == "") {
+          errors.message = "El campo foto de perfil está vacío";
           res.locals.errors = errors;
           res.render("register");
-        } else if (req.body.email == "") {
-          errors.message = "El campo email está vacío";
+      } else if (req.body.birthdate =="") {
+          errors.message = "El campo fecha de nacimiento esta vacio.";
           res.locals.errors = errors;
           res.render("register");
-        } else if (req.body.password =="") {
-          errors.message = "La contraseña debe tener mas de 3 caracteres.";
+      } else if (req.body.dni =="") {
+          errors.message = "El campo DNI esta vacio.";
           res.locals.errors = errors;
           res.render("register");
-        } else if (req.body.perfil == "") {
-            errors.message = "El campo foto de perfil está vacío";
+      } else {
+        let criterio = {
+          where: [{ email: req.body.email }]
+        }
+        Usuario.findAll(criterio)
+          .then(data => {
+            if (data != null) {
+            errors.message = "El email ya existe!";
             res.locals.errors = errors;
             res.render("register");
-        } else if (req.body.birthdate =="") {
-            errors.message = "El campo fecha de nacimiento esta vacio.";
-            res.locals.errors = errors;
-            res.render("register");
-        } else if (req.body.dni =="") {
-            errors.message = "El campo DNI esta vacio.";
-            res.locals.errors = errors;
-            res.render("register");
-        } else  {
-          let criterio = {
-            where: [{ email: req.body.email }]
-          }
-          Usuario.findAll(criterio)
-            .then(function(data){
-              errors.message = "El email ya existe!";
-              res.locals.errors = errors;
-              res.render("register");
-            }).catch(error => console.log(error))
-    
+            }
+          }).catch(error => console.log(error))
+
           let passEncriptada = bcryptjs.hashSync(req.body.password, 12);
           let user = {
             username:req.body.username,
@@ -56,11 +58,11 @@ const usersController = {
             foto_perfil: req.body.perfil,
             dni: req.body.dni,
             fecha_nacimiento: req.body.birthdate
-        }
-        Usuario.create(user);
-        res.redirect('register')
-        }
-      },
+          }
+          Usuario.create(user);
+          res.redirect('/register')
+          }
+    },
       login: (req, res) => {
         res.render('login')
       },
